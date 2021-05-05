61ec0a8d0d602d9e357c861bb1dd485c47fff2df
Zorigto Badmain <badmainzzz@mail.ru>
2021-05-05 14:53:11 +0900

    Initial Commit

---
+++ Er-Telecom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Er-Telecom
+//
+//  Created by Зоригто Бадмаин on 05.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
